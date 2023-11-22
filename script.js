8c66895660f685356a209adc065d75c9faa1aa4e
Fred-Khan <gokart_driver@yahoo.com.au>
2023-11-23 10:59:03 +1100

    Initial Commit.

---
+++ script.js
@@ -0,0 +1,31 @@
+// Wait for the HTML to be fully loaded before running the JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    // Generate a random number between 1 and 100
+
+    // Get references to important elements in the HTML
+
+    // Listen for a click on the submit button
+    submitBtn.addEventListener("click", function() {
+        // Get the user's guess from the input field and convert it to a number
+
+        // Check if the user's input is valid
+
+            // Display an error message if the input is not a valid number
+
+            // If input is valid, compare the user's guess with the random number
+
+                // If the guess is correct, display a success message
+  
+                // Change the color of the success message to green
+
+                // Disable the input field and submit button to prevent further guesses
+
+
+                // If the guess is too low, provide a hint to guess higher
+
+                // If the guess is too high, provide a hint to guess lower
+
+            }
+        }
+    });
+});
